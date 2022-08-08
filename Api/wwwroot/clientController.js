7416c6dbe30c819139ea1034ba6d213ce8f881ad
Victor Siqueira Ribeiro <victor.ribeiro@comlink.com.br>
2022-08-08 17:26:00 -0300

    Melhorando API e criando o Front End para consumo da API utilizando Angular

--- Api/wwwroot/clientController.js
+++ Api/wwwroot/clientController.js
@@ -3,7 +3,9 @@ app.controller('clientController', ['$scope', '$http', clientController]);
 
 function clientController($scope, $http) {
     $http.get('https://localhost:44315/api/Item/getAllItems').success(function (data) {
+        console.log(data);
         $scope.listagemItems = data;
+        console.log(data);
     }).error(function () {
         $scope.erro = "Erro ao exibir a listagem de itens.";
     });
